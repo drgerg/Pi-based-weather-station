89ba1331024d4b397a480dddc93b0e7358e06a83
Gregory Sanders <46494738+casspop@users.noreply.github.com>
2020-03-29 06:53:34 -0500

    add GPL3.0 text

--- Code/out/outBgFan.py
+++ Code/out/outBgFan.py
@@ -1,10 +1,23 @@
 #!/usr/bin/env python3
-""" outBgFan.py - Manage the 4-Wire Delta Electronics KDB050510HB recycled laptop cooling fan.
-        This is intended to run as a standalone service to manage the speed of the fan based on the 
-        CPU temperature.
-    2019,2020 - Gregory Allen Sanders."""
+#   outBgFan.py - Manage the 4-Wire Delta Electronics KDB050510HB recycled laptop cooling fan.
+#                 This is intended to run as a standalone service to manage the speed of the fan based on the 
+#                 CPU temperature.
+#     Copyright (c) 2019,2020 - Gregory Allen Sanders.
 
-import time,os,logging,signal,sys,argparse,pickle
+#     This program is free software: you can redistribute it and/or modify
+#     it under the terms of the GNU General Public License as published by
+#     the Free Software Foundation, either version 3 of the License, or
+#     (at your option) any later version.
+
+#     This program is distributed in the hope that it will be useful,
+#     but WITHOUT ANY WARRANTY; without even the implied warranty of
+#     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#     GNU General Public License for more details.
+
+#    You should have received a copy of the GNU General Public License
+#     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import time,os,logging,signal,sys,argparse,pickle,traceback
 from time import sleep
 import RPi.GPIO as GPIO
 
