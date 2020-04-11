3b0515d009097c6e6eca31fb50f43d593dddca5a
Gregory Sanders <46494738+casspop@users.noreply.github.com>
2020-04-11 08:43:27 -0500

    Update outMainDATA.py

--- Code/out/outMainDATA.py
+++ Code/out/outMainDATA.py
@@ -17,36 +17,11 @@
 #    You should have received a copy of the GNU General Public License
 #     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
-import datetime,time,bme280,rainMain,windSpd,outFan,os,logging,signal,sys,statistics,mysql.connector,configparser,pickle
+import datetime,time,argparse,bme280,rainMain,windSpd,outFan,os,logging,signal,sys,statistics,mysql.connector,configparser,pickle
 import windDir as wDirection
-import argparse
 from time import sleep
 import RPi.GPIO as GPIO
 from mysql.connector import MySQLConnection, Error
-
-parserOm = argparse.ArgumentParser()
-parserOm.add_argument("-d", "--debug", help="Turn on debugging output to log file.", action="store_true")
-OmHome = os.getcwd()
-logger = logging.getLogger(__name__)
-#
-config = configparser.RawConfigParser()
-config.read(OmHome + '/out.conf')
-#
-argsOm = parserOm.parse_args()
-
-if argsOm.debug:
-    import traceback
-    logging.basicConfig(filename=OmHome + '/outMain.log', format='[%(name)s]:%(levelname)s: %(message)s. - %(asctime)s', datefmt='%D %H:%M:%S', level=logging.DEBUG)
-    logging.info("Debugging output enabled")
-else:
-    logging.basicConfig(filename=OmHome + '/outMain.log', format='%(asctime)s - %(message)s', datefmt='%a, %d %b %Y %H:%M:%S', level=logging.INFO)
-#
-logger.info(" - - - - DATA LOGGING STARTED - - - - ")
-logger.info("  INITIAL CONFIGURATION COMPLETE  ")
-logger.info("'HOME' path is: " + OmHome)
-
-
-
 
 wsPin = 16
 rps = 0
@@ -56,6 +31,8 @@ rpulse = 0
 relapse = 0
 rstart = 0
 accResults = []
+pklData = []
+tableData = []
 
 pressure = 0
 outTemp = 0
@@ -105,7 +82,7 @@ def datagrabber():  # Setup for Data Logging
         accWspdK.append(float(kph))
         accWspdM.append(float(mph))
         accWdeg.append(wdeg)
-        if argsOm.debug:
+        if argsOMD.debug:
             print(str(time.time() - start) + ': Wind Deg: ' + str(wdeg) + ' Wind Speed: ' + str(mph))
     pressure = statistics.median(accPress)               ## Get the middle value in the list of values
     outTemp = statistics.median(accTemp)                 ## accumulated over the last 60 seconds
@@ -157,8 +134,6 @@ def wDirCvt(wdegval):
     if 326.26 <= val <= 348.75:
             winddirS = 'NNW'
     return winddirS
-
-
 #
 # A calculation to approximate the dewpoint when humidity is over 50%
 # Td = T - ((100 - RH)/5.)
@@ -177,9 +152,6 @@ def wDirCvt(wdegval):
 #   rainRate
 #   rain
 #   extraTemp1 = CPU temp
-#   
-#   
-
 #   
 #   I'm going to use the prefix 's_' to indicate a 'samples' list variable.  So 'outTemp' becomes 's_outTemp'.
 #   's_outTemp' contains the list of values accumulated during this one-shot gathering episode.
@@ -208,11 +180,21 @@ def bme():
     return outTemp,pressure,outHumidity,extraHumid1
 #
 ## START WRITE-TO-DATABASE FUNCTION
+# Be aware that because we are using WiFi to connect to the network, we have to make arrangements in case
+# the connection drops.  It happens.  So we are getting sensor data, putting it in a list, checking for a .pkl file
+# which will exist if the connection test failed last time around, appending our new data to the data from that .pkl file,
+# ping the SQL server machine, if good: write rows of data from our list and delete the .pkl file. If not good: dump the appended list to 
+# the .pkl file (which overwrites it if it existed) and then swing back around to do it all again.
 #
 def mydb():
+    global pklData
+    # Get current values from all the sensors.
     pressure,outTemp,outHumidity,extraHumid1,windSpeed,winddir,wdirStr = datagrabber()
-    fan1 = pickle.load(open(OmHome + '/fanSpd.pkl', 'rb'))
+    # Get the vent fan RPM from the pickle file.
+    fan1 = pickle.load(open(OMDHome + '/fanSpd.pkl', 'rb'))
+    # Get the CPU temperature from the system.
     cpuT = cpuTemp()[1]
+    # Now make sure all the variables are of the right type.
     prs = float(pressure)
     otmp = float(outTemp)
     ohmd = float(outHumidity)
@@ -220,41 +202,84 @@ def mydb():
     wspd = float(windSpeed)
     wdir = float(winddir)
     wdirStr = wdirStr
-    funNStr = sys._getframe().f_code.co_name
-    logger.debug("Started the " + funNStr + " function")
     dtNow = int(time.time())
-    DBhost=config.get('mySQL','Address')
+    dataTable = []                                          ## Create a new blank list (dunno why I insist on calling these tables)
+    dataTable.append(dtNow)                                 # append all the variables one by one
+    dataTable.append(prs)
+    dataTable.append(otmp)
+    dataTable.append(ohmd)
+    dataTable.append(wspd)
+    dataTable.append(wdir)
+    dataTable.append(wdirStr)
+    dataTable.append(xohmd)
+    dataTable.append(cpuT)
+    dataTable.append(fan1)                                  # OK.  That's all our fresh data in one list.
+    if os.path.exists(OMDHome + '/allData.pkl'):            ## This .pkl file will exist if the upcoming ping test failed last time around.
+        pklData = pickle.load(open(OMDHome + '/allData.pkl', 'rb'))
+    pklData.append(dataTable)                               ## Merge the recent data with the .pkl file data just in case we are still offline
+    logger.debug('dataTable was appended to pklData')       
+    DBhost=config.get('mySQL','Address')                    # get the mySQL login data from our config file.
     DBuser=config.get('mySQL','User')
     DBpasswd=config.get('mySQL','Password')
     DBdatabase=config.get('mySQL','Database')
     DBtable=config.get('mySQL','Table1')
-    mydb = mysql.connector.connect(
-        host=DBhost,
-        user=DBuser,
-        passwd=DBpasswd,
-        database=DBdatabase
-    )
-    cursor = mydb.cursor()
-    cursor.execute("select database();")
-    record = cursor.fetchone()
-    logger.debug("Got the database: " + str(record))
-    try:
-        addRecord = ('INSERT INTO ' + DBdatabase + '.' + DBtable + '' \
-            ' (dateTime,pressure,outTemp,outHumidity,windSpeed,winddir,wdirStr,extraHumid1,cpuTemp,fan1)' \
-            ' VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)')
-        addData = (dtNow,prs,otmp,ohmd,wspd,wdir,wdirStr,xohmd,cpuT,fan1)
-        logger.debug(addRecord %addData)
-        cursor.execute(addRecord,addData)
-        cursor.execute("SELECT * FROM " + DBdatabase + '.' + DBtable + " where id=(select max(id) from " + DBdatabase + '.' + DBtable + ")")
-        record = cursor.fetchone()
-        logger.debug('The last record contains: ' + str(record))
-        mydb.commit()
-        cursor.close()
-        mydb.close()
-    except Exception:
-        logger.info('Fatal Error in mydb()', exc_info=True)
-    funNStr = sys._getframe().f_code.co_name
-    logger.debug("Finished the " + funNStr + " function")
+    # Don't stop now . . . keep going!!!
+    ## Check to see if we can ping the machine mySQL is running on
+    numBadPings = 0
+    pingRes = os.system("ping -c 1 " + DBhost + " > /dev/null 2>&1")  ## Ping test to make sure the SQL machine is there.
+    logger.debug("Ping for server returned: " + str(pingRes))
+    if pingRes == 0:                                        # We are connected.  Move ahead.  If not, don't do any of this stuff.
+        numBadPings = 0                                     # Prep the data, and send it to the SQL machine.
+        for row in pklData:                                 # Normally there will only be one row. UNLESS the network was down, 
+            dtNow = row[0]                                  # in which case there can be many.
+            prs = row[1]
+            otmp = row[2]
+            ohmd = row[3]
+            wspd = row[4]
+            wdir = row[5]
+            wdirStr = row[6]
+            xohmd = row[7]
+            cpuT = row[8]
+            fan1 = row[9]
+            try:
+                # prepare the database connector 
+                mydb = mysql.connector.connect(
+                    host=DBhost,
+                    user=DBuser,
+                    passwd=DBpasswd,
+                    database=DBdatabase
+                )
+                cursor = mydb.cursor()
+                cursor.execute("select database();")
+                record = cursor.fetchone()
+                logger.debug("Got the database: " + str(record))
+                addRecord = ('INSERT INTO ' + DBdatabase + '.' + DBtable + '' \
+                    ' (dateTime,pressure,outTemp,outHumidity,windSpeed,winddir,wdirStr,extraHumid1,cpuTemp,fan1)' \
+                    ' VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)')
+                addData = (dtNow,prs,otmp,ohmd,wspd,wdir,wdirStr,xohmd,cpuT,fan1)
+                logger.debug(addRecord %addData)
+                cursor.execute(addRecord,addData)
+                cursor.execute("SELECT * FROM " + DBdatabase + '.' + DBtable + " where id=(select max(id) from " + DBdatabase + '.' + DBtable + ")")
+                record = cursor.fetchone()
+                logger.debug('The last record contains: ' + str(record))
+                mydb.commit()
+                cursor.close()
+                mydb.close()
+                pklData = []
+                if os.path.exists(OMDHome + '/allData.pkl'):
+                    os.remove(OMDHome + '/allData.pkl')
+                    logger.debug("Pickle file erased.")
+            except Exception:
+                logger.info('Fatal Error in mydb()', exc_info=True)
+                pass
+            funNStr = sys._getframe().f_code.co_name
+            logger.debug("Finished the " + funNStr + " function")
+    else:                                                           # We are NOT CONNECTED.  Save our readings to a .pkl file
+        logger.info("Ping test for SQL server machine failed.")
+        numBadPings += 1
+        pickle.dump(pklData, open(OMDHome + '/allData.pkl', 'wb+'), pickle.HIGHEST_PROTOCOL)
+        logger.debug("allData.pkl updated.")
+        logger.info ("Number of bad pings is: " + str(numBadPings) + ".")
 #
 ##  END OF WRITE-TO-DATABASE FUNCTION
 #
@@ -312,24 +337,50 @@ def SignalHandler(signal, frame):
         logger.debug("Wrote to log in SignalHandler")
         logger.info("Finished SignalHandler")
         logger.info("That's all folks.  Goodbye")
-        logger.info(" - - - - DATA LOGGING STOPPED - - - - ")
+        logger.info(" - - - - DATA LOGGING STOPPED INTENTIONALLY - - - - ")
         sys.exit(0)
 
 if __name__ == "__main__":
-        import traceback
-        try:
-            signal.signal(signal.SIGINT, SignalHandler)  ## This one catches CTRL-C from the local keyboard
-            signal.signal(signal.SIGTERM, SignalHandler) ## This one catches the Terminate signal from the system
-            logger.info(" Top of try")
-            while True:
-                mydb()
-            pass
+    #
+    ## Arg parsing moved down here so we can use functions from this file as a module without conflicting the commandline arguments.
+    #
+    parserOMD = argparse.ArgumentParser()
+    parserOMD.add_argument("-d", "--debug", help="Turn on debugging output to log file.", action="store_true")
+    OMDHome = os.getcwd()
+    logger = logging.getLogger(__name__)
+    #
+    config = configparser.RawConfigParser()
+    config.read(OMDHome + '/out.conf')
+    #
+    argsOMD = parserOMD.parse_args()
 
-            logger.info("Bottom of try")
+    if argsOMD.debug:
+        import traceback
+        logging.basicConfig(filename=OMDHome + '/outMain.log', format='[%(name)s]:%(levelname)s: %(message)s. - %(asctime)s', datefmt='%D %H:%M:%S', level=logging.DEBUG)
+        logging.info("Debugging output enabled")
+    else:
+        logging.basicConfig(filename=OMDHome + '/outMain.log', format='%(asctime)s - %(message)s', datefmt='%a, %d %b %Y %H:%M:%S', level=logging.INFO)
+    #
+    logger.info(" - - - - DATA LOGGING STARTED - - - - ")
+    logger.info("  INITIAL CONFIGURATION COMPLETE  ")
+    logger.info("'HOME' path is: " + OMDHome)
+    #
+    ## END Args parsing 
+    #
+    import traceback
+    try:
+        signal.signal(signal.SIGINT, SignalHandler)  ## This one catches CTRL-C from the local keyboard
+        signal.signal(signal.SIGTERM, SignalHandler) ## This one catches the Terminate signal from the system
+        logger.info(" Top of try")
+        while True:
+            mydb()
+        pass
+
+        logger.info("Bottom of try")
 #            logger.flush()
-        except Exception:
-            logger.info("Exception caught at bottom of try.", exc_info=True)
-            error = traceback.print_exc()
-            logger.info(error)
-            logger.info("That's all folks.  Goodbye")
-            logger.info(" - - - - DATA LOGGING STOPPED BY EXCEPTION - - - - ")
+    except Exception:
+        logger.info("Exception caught at bottom of try.", exc_info=True)
+        error = traceback.print_exc()
+        logger.info(error)
+        logger.info("That's all folks.  Goodbye")
+        logger.info(" - - - - DATA LOGGING STOPPED BY EXCEPTION - - - - ")
